60329bc102670acf406d6f2ce74a54251719e522
KseniiaViushkova <kseniia_viushkova@epam.com>
2019-03-11 23:22:18 +0300

    add steps 5 through 7 in hw1 hard asserts test

--- src/test/java/hw1/BasicIndexPageHardAssertsTest.java
+++ src/test/java/hw1/BasicIndexPageHardAssertsTest.java
@@ -2,12 +2,15 @@ package hw1;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class BasicIndexPageHardAssertsTest {
 
@@ -36,8 +39,25 @@ public class BasicIndexPageHardAssertsTest {
         assertEquals(driver.findElement(By.id("user-name")).getText(), "PITER CHAILOVSKII");
 
         //5 Assert Browser title
+        assertEquals(driver.getTitle(), "Home Page");
+
         //6 Assert that there are 4 items on the header section are displayed and they have proper texts
+        String[] navItems = {"HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS"};
+        for (int i = 0; i < navItems.length; i++) {
+            WebElement navElem = driver.findElement(By.cssSelector(".uui-header  .uui-navigation.nav > li:nth-child("
+                    + (i + 1) + ") a"));
+            String navText = navElem.getText();
+            assertTrue(navElem.isDisplayed());
+            assertEquals(navText, navItems[i]);
+        }
+
         //7 Assert that there are 4 images on the Index Page and they are displayed
+        List<WebElement> images = driver.findElements(By.cssSelector(".icons-benefit"));
+        assertEquals(images.size(), 4);
+        for (WebElement image : images) {
+            assertTrue(image.isDisplayed());
+        }
+
         //8 Assert that there are 4 texts on the Index Page under icons and they have proper text
         //9 Assert a text of the main headers
         //10 Assert that there is the iframe in the center of page
