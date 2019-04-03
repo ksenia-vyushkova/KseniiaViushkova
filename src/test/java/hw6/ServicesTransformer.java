f3b91c25b82767283f3907aaa8efb0aa63ae84dc
Kseniia_Viushkova <kseniia_viushkova@epam.com>
2019-04-03 17:04:47 +0300

    refactor hw6, get rid of switch in ServicesTransformer

--- src/test/java/hw6/ServicesTransformer.java
+++ src/test/java/hw6/ServicesTransformer.java
@@ -8,8 +8,6 @@ import io.cucumber.datatable.DataTableType;
 import java.util.Locale;
 import java.util.Map;
 
-import static enums.NavService.*;
-
 public class ServicesTransformer implements TypeRegistryConfigurer {
     @Override
     public Locale locale() {
@@ -21,41 +19,7 @@ public class ServicesTransformer implements TypeRegistryConfigurer {
         typeRegistry.defineDataTableType(new DataTableType(NavService.class,
                         (Map<String, String> row) -> {
                             String service = row.get("Services");
-                            NavService navService;
-                            switch (service) {
-                                case ("SUPPORT"):
-                                    navService = SUPPORT;
-                                    break;
-                                case ("DATES"):
-                                    navService = DATES;
-                                    break;
-                                case ("SEARCH"):
-                                    navService = SEARCH;
-                                    break;
-                                case ("COMPLEX_TABLE"):
-                                    navService = COMPLEX_TABLE;
-                                    break;
-                                case ("SIMPLE_TABLE"):
-                                    navService = SIMPLE_TABLE;
-                                    break;
-                                case ("USER_TABLE"):
-                                    navService = USER_TABLE;
-                                    break;
-                                case ("TABLE_WITH_PAGES"):
-                                    navService = TABLE_WITH_PAGES;
-                                    break;
-                                case ("DIFFERENT_ELEMENTS"):
-                                    navService = DIFFERENT_ELEMENTS;
-                                    break;
-                                case ("PERFORMANCE"):
-                                    navService = PERFORMANCE;
-                                    break;
-                                default:
-                                    navService = null;
-                                    break;
-                            }
-
-                            return navService;
+                            return NavService.valueOf(service);
                         }
                 )
         );
